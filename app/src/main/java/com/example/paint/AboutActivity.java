de55b1b50a87cfb9fbc5b7e20bf3b258c97c1fa6
pedromarques <pedro.jam@hotmail.com>
2020-10-04 22:10:41 +0100

    Project half way through: splash screen, initial menu, about menu, settings menu on the making

---
+++ app/src/main/java/com/example/paint/AboutActivity.java
@@ -0,0 +1,25 @@
+package com.example.paint;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        Toolbar toolbar = findViewById(R.id.aboutActivityToolbar);
+        setSupportActionBar(toolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+}
