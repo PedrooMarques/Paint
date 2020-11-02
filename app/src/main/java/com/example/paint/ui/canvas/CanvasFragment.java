8ff7a048744ba807f91b31d265e3433d2945aa12
pedromarques <pedro.jam@hotmail.com>
2020-11-02 02:50:38 +0000

    working shake detector but i wanna try something else: STABLE

--- app/src/main/java/com/example/paint/ui/canvas/CanvasFragment.java
+++ app/src/main/java/com/example/paint/ui/canvas/CanvasFragment.java
@@ -2,11 +2,16 @@ package com.example.paint.ui.canvas;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,6 +26,49 @@ import com.example.paint.ui.palette.PaletteViewModel;
 
 public class CanvasFragment extends Fragment {
 
+    private static final float SHAKE_THRESHOLD_GRAVITY = 1.9F;
+    private static final int SHAKE_SLOP_TIME_MS = 1000;
+    // sensor event listener
+    private final SensorEventListener sensorEventListener = new SensorEventListener() {
+
+        private long mShakeTimestamp;
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+            if (event.sensor.getType() != Sensor.TYPE_LINEAR_ACCELERATION)
+                return;
+
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            float gX = x / SensorManager.GRAVITY_EARTH;
+            float gY = y / SensorManager.GRAVITY_EARTH;
+            float gZ = z / SensorManager.GRAVITY_EARTH;
+
+            // gForce will be close to 1 when there is no movement.
+            float gForce = (float) Math.sqrt(gX * gX + gY * gY + gZ * gZ);
+            if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+                final long now = System.currentTimeMillis();
+                // ignore shake events too close to each other (500ms)
+                if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
+                    return;
+                }
+
+                mShakeTimestamp = now;
+
+                Toast.makeText(getContext(), "penis", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+    private SensorManager sensorManager;
+
     private CanvasViewModel mCanvasViewModel;
     private PaletteViewModel mPaletteSharedViewModel;
 
@@ -30,18 +78,27 @@ public class CanvasFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_canvas, container, false);
     }
 
+    private Sensor mLAccelerometer;
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        // view models
         mCanvasViewModel = new ViewModelProvider(this).get(CanvasViewModel.class);
         mPaletteSharedViewModel = new ViewModelProvider(requireActivity()).get(PaletteViewModel.class);
 
+        // sensors
+        sensorManager = (SensorManager) requireActivity().getSystemService(Context.SENSOR_SERVICE);
+        mLAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+        // touch events
         GestureListener mGestureListener = new GestureListener();
         GestureDetector mGestureDetector = new GestureDetector(getContext(), mGestureListener);
         mGestureDetector.setIsLongpressEnabled(true);
         mGestureDetector.setOnDoubleTapListener(mGestureListener);
 
+        // shared preferences
         SharedPreferences sharedPref = requireContext().getSharedPreferences(
                 requireContext().getString(R.string.preference_file_key), Context.MODE_PRIVATE);
         int defaultValue = 0;
@@ -72,5 +129,17 @@ public class CanvasFragment extends Fragment {
         mPaletteSharedViewModel.getBrushColor().observe(getViewLifecycleOwner(), paintCanvas::setBrushColor);
 
         mCanvasViewModel.getCanvasColor().observe(getViewLifecycleOwner(), paintCanvas::setCanvasColor);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(sensorEventListener, mLAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(sensorEventListener);
     }
 }
